39774550a6ea724fd7bfc10e8666feb72d812565
Mikaela Matias <maria_angela_mikaela_matias@dlsu.edu.ph>
2021-04-07 14:44:36 +0800

    file reading

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-  printf("Hello World\n");
+int main() {
+  char fileName[100];
+  FILE: *fp;
   
+  printf("Enter filename: ");
+  scanf("%s", fileName);
+
+  fp = fopen(fileName,"r");
+  
+  if (fp == NULL) {
+    printf ("%s not found.\n", fileName);
+    exit(0);
+  } else {
+    
+  }
   return 0;
 }
